43fd4b87e6892e89788649a0df3092a9301767e1
Aluiel <heaven99902@gmail.com>
2025-02-07 05:02:17 +0200

    views.py: - Added task status variables to context in load_tasks_view. - Updated add_task_view: added validation for priority and deadline. - Implemented update_task_view to handle task updates. - Created view for rendering task_update_form.html.
    Templates:
    - Updated add_task_form: added fields for priority and deadline.
    - Added update_task_form for task editing.
    - Refactored tasks_list: replaced list with a table for better readability.

--- task_manager_app/urls.py
+++ task_manager_app/urls.py
@@ -1,5 +1,4 @@
 from django.urls import path
-from django.contrib.auth.decorators import login_required
 from .views import *
 
 urlpatterns = [
@@ -10,7 +9,7 @@ urlpatterns = [
     path('delete-project/<int:project_id>/', delete_project_view, name="delete_project"),
     path('load-tasks/<int:project_id>/', load_tasks_view, name="load_tasks"),
     path("add-task/<int:project_id>/", add_task_view, name="add_task"),
-    # path('update-task/<int:task_id>/', update_task_view, name="update_task"),
-    # path('change-priority-task/<int:task_id>/', change_priority_task_view, name="change_priority_task"),
+    path('update-task/<int:task_id>/', update_task_view, name="update_task"),
+    path('get-update-task-form/<int:task_id>/', get_update_task_form_view, name="get_task_update_form"),
     path('delete-task/<int:task_id>/', delete_task_view, name="delete_task"),
 ]
--- task_manager_app/views.py
+++ task_manager_app/views.py
@@ -1,23 +1,22 @@
 from django.contrib.auth.decorators import login_required
-from django.views.generic import ListView
 from django.http import HttpResponseBadRequest, HttpResponseForbidden
 from django.shortcuts import render, get_object_or_404
 from .models import Project, Task
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.views.generic import TemplateView
+from datetime import datetime
 
 
 # Create your views here.
 class HomeView(LoginRequiredMixin, TemplateView):
     template_name = "task_manager_app/home.html"
 
-
+@login_required
 def load_projects_view(request):
     projects = Project.objects.filter(user=request.user)
     return render(request, "task_manager_app/projects_list.html", {"projects": projects})
 
-# add_project view for the version where the project list logic
-# works based on individual project_item templates instead of reloading the entire list.
+
 @login_required
 def add_project_view(request):
     if request.method == "POST":
@@ -29,6 +28,7 @@ def add_project_view(request):
 
         return render(request, "task_manager_app/project_item.html", {"project": project})
 
+@login_required
 def update_project_view(request, project_id):
     project = get_object_or_404(Project, id=project_id, user=request.user)
 
@@ -43,7 +43,7 @@ def update_project_view(request, project_id):
         projects = Project.objects.filter(user=request.user)
         return render(request, "task_manager_app/projects_list.html", {"projects": projects})
 
-
+@login_required
 def delete_project_view(request, project_id):
     if request.method == "POST":
         project = get_object_or_404(Project, pk=project_id, user=request.user)
@@ -57,29 +57,74 @@ def delete_project_view(request, project_id):
 def load_tasks_view(request, project_id):
     project = get_object_or_404(Project, id=project_id, user=request.user)
     tasks = project.tasks.all()
-    return render(request, "task_manager_app/tasks_list.html", {"tasks": tasks, "project": project})
+    priority_choices = Task.PRIORITY_CHOICES
+    task_status_choices = Task.STATUS_CHOICES
+    return render(request, "task_manager_app/tasks_list.html", {"tasks": tasks, "project": project,
+                                                                "priority_choices": priority_choices,
+                                                                "task_status_choices": task_status_choices,
+                                                                "now":datetime.now()})
 
-
+@login_required
 def add_task_view(request, project_id):
     if request.method == "POST":
         project = get_object_or_404(Project, id=project_id, user=request.user)
         task_name = request.POST.get("name")
-
+        task_priority = request.POST.get("priority")
+        task_deadline = request.POST.get("deadline")
+        priority_choices = Task.PRIORITY_CHOICES
+        task_status_choices = Task.STATUS_CHOICES
         if not task_name:
             return HttpResponseBadRequest("Task name is required.")
 
-        Task.objects.create(name=task_name, project=project)
+        if task_priority and task_priority.isdigit():
+            task_priority = int(task_priority)
+
+        if task_deadline:
+            task_deadline = datetime.strptime(task_deadline, "%Y-%m-%dT%H:%M")
+        else:
+            task_deadline = None
+
+        Task.objects.create(name=task_name, project=project, priority=task_priority, deadline=task_deadline)
 
         tasks = project.tasks.all()
-        return render(request, "task_manager_app/tasks_list.html", {"tasks": tasks, "project": project})
-#
-#
-# def update_task_view(request):
-#     pass
-#
-# def change_priority_task_view(request):
-#     pass
-#
+        return render(request, "task_manager_app/tasks_list.html", {"tasks": tasks,
+                                                                    "project": project,
+                                                                    'priority_choices': priority_choices,
+                                                                    'task_status_choices': task_status_choices,
+                                                                    "now": datetime.now()})
+
+@login_required
+def update_task_view(request, task_id):
+    if request.method == "POST":
+        task_name = request.POST.get("name")
+        task_priority = request.POST.get("priority")
+        task_deadline = request.POST.get("deadline")
+        task_status = request.POST.get("status")
+
+        task = get_object_or_404(Task, id=task_id)
+        project = get_object_or_404(Project, id=task.project.id)
+        if (task_name, task_priority, task_status, task_deadline) != (task.name, task.priority, task.status, task.deadline):
+            task.name = task_name
+            task.priority = int(task_priority)
+            task.status = task_status
+            task.deadline = datetime.strptime(task_deadline, "%Y-%m-%dT%H:%M") if task_deadline != "" else None
+            task.save()
+            tasks = project.tasks.all()
+            return render(request, "task_manager_app/tasks_list.html", {
+                "tasks": tasks,
+                "project": project,
+                "priority_choices": Task.PRIORITY_CHOICES,
+                "task_status_choices": Task.STATUS_CHOICES,
+                "now": datetime.now()
+            })
+
+
+
+def get_update_task_form_view(request, task_id):
+    task = get_object_or_404(Task, id=task_id)
+
+    return render(request, "task_manager_app/update_task_form.html", {"task": task, "now":datetime.now()})
+
 @login_required
 def delete_task_view(request, task_id):
     if request.method == "POST":
