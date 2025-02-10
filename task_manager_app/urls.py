80df688d688cc4cfafa1c8b0f0516a3a41402e5a
Aluiel <heaven99902@gmail.com>
2025-02-10 07:35:22 +0200

    urls.py - Update import views to improve readability.

--- task_manager_app/urls.py
+++ task_manager_app/urls.py
@@ -1,15 +1,15 @@
 from django.urls import path
-from .views import *
+import task_manager_app.views as views
 
 urlpatterns = [
-    path('', HomeView.as_view(), name='home'),
-    path("load-projects/", load_projects_view, name="load_project_list"),
-    path("add-project/", add_project_view, name="add_project"),
-    path('update-project/<int:project_id>/', update_project_view, name="update_project"),
-    path('delete-project/<int:project_id>/', delete_project_view, name="delete_project"),
-    path('load-tasks/<int:project_id>/', load_tasks_view, name="load_tasks"),
-    path("add-task/<int:project_id>/", add_task_view, name="add_task"),
-    path('update-task/<int:task_id>/', update_task_view, name="update_task"),
-    path('get-update-task-form/<int:task_id>/', get_update_task_form_view, name="get_task_update_form"),
-    path('delete-task/<int:task_id>/', delete_task_view, name="delete_task"),
+    path('', views.HomeView.as_view(), name='home'),
+    path("load-projects/", views.load_projects_view, name="load_project_list"),
+    path("add-project/", views.add_project_view, name="add_project"),
+    path('update-project/<int:project_id>/', views.update_project_view, name="update_project"),
+    path('delete-project/<int:project_id>/', views.delete_project_view, name="delete_project"),
+    path('load-tasks/<int:project_id>/', views.load_tasks_view, name="load_tasks"),
+    path("add-task/<int:project_id>/", views.add_task_view, name="add_task"),
+    path('update-task/<int:task_id>/', views.update_task_view, name="update_task"),
+    path('get-update-task-form/<int:task_id>/', views.get_update_task_form_view, name="get_task_update_form"),
+    path('delete-task/<int:task_id>/', views.delete_task_view, name="delete_task"),
 ]
